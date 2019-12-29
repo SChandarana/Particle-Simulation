edfbde183ac32cd8cc2a72db9e8dd2c29a2d00b0
SChandarana <44266225+SChandarana@users.noreply.github.com>
2019-12-29 17:38:01 +0000

    changing flie structure

--- step3 attempts old/solution-step3-dyanamic.c
+++ step3 attempts old/solution-step3-dyanamic.c
@@ -180,60 +180,7 @@ void printParaviewSnapshot() {
  */
  //used to check is the previous iteration had a collision
 void updateBody() {
-  const int bucketNumber = 10;
-  int** buckets = new int*[bucketNumber](); //initialise a pointer to pointers for all [bucketNumber] buckets
-  for(int i=0; i<bucketNumber; i++){
-    buckets[i] = new int[NumberOfBodies](); //each bucket can hold up to [NumberOfBodies] values
-  }
-  int* bucketPointers = new int[bucketNumber]();
-  
-  if (!(t>0)){
-    for(int i=0; i<NumberOfBodies;i++){
-      buckets[0][bucketPointers[0]] = i;
-      bucketPointers[0] += 1;
-    }
-  }else{
-    double vBucket = maxV/bucketNumber;
-    for (int i=0; i<NumberOfBodies; i++){
-      double velocity = std::sqrt( v[i][0]*v[i][0] + v[i][1]*v[i][1] + v[i][2]*v[i][2] );
-      if(velocity <= vBucket){
-        buckets[0][bucketPointers[0]] = i;
-        bucketPointers[0] += 1;
-      } else if (velocity <= vBucket * 2){
-        buckets[1][bucketPointers[1]] = i;
-        bucketPointers[1] += 1;
-      } else if (velocity <= vBucket * 3){
-        buckets[2][bucketPointers[2]] = i;
-        bucketPointers[2] += 1;
-      } else if (velocity <= vBucket * 4){
-        buckets[3][bucketPointers[3]] = i;
-        bucketPointers[3] += 1;
-      } else if (velocity <= vBucket * 5){
-        buckets[4][bucketPointers[4]] = i;
-        bucketPointers[4] += 1;
-      } else if (velocity <= vBucket * 6){
-        buckets[5][bucketPointers[5]] = i;
-        bucketPointers[5] += 1;
-      } else if (velocity <= vBucket * 7){
-        buckets[6][bucketPointers[6]] = i;
-        bucketPointers[6] += 1;
-      } else if (velocity <= vBucket * 8){
-        buckets[7][bucketPointers[7]] = i;
-        bucketPointers[7] += 1;
-      } else if (velocity <= vBucket * 9){
-        buckets[8][bucketPointers[8]] = i;
-        bucketPointers[8] += 1;
-      } else {
-        buckets[9][bucketPointers[9]] = i;
-        bucketPointers[9] += 1;
-      }
-    }
-  }
-  
-  static bool prevCol = false; //used to check is the previous iteration had a collision
-  const double threshold = 0.01*0.01;
-  maxV   = 0.0;
-  minDx  = std::numeric_limits<double>::max();
+  static bool prevCol = false;
   // force0 = force along x direction
   // force1 = force along y direction
   // force2 = force along z direction
@@ -245,64 +192,55 @@ void updateBody() {
   static double* prevf1 = new double[NumberOfBodies]();
   static double* prevf2 = new double[NumberOfBodies]();
   
+  int bucketNumber = 10;
+  int* bucket = new int[NumberOfBodies]();
 
-  for(int k=0; k<bucketNumber; k++){
-    int timesteps = pow(2,k);
+  const double vBucket = maxV / bucketNumber;
+  for(int i=0; i<NumberOfBodies; i++){
+    double velocity = std::sqrt( v[i][0]*v[i][0] + v[i][1]*v[i][1] + v[i][2]*v[i][2] );
+    if(velocity <= vBucket){
+      bucket[i] = 0;
+    } else if (velocity <= vBucket * 2){
+      bucket[i] = 1;
+    } else if (velocity <= vBucket * 3){
+      bucket[i] = 2;
+    } else if (velocity <= vBucket * 4){
+      bucket[i] = 3;
+    } else if (velocity <= vBucket * 5){
+      bucket[i] = 4;
+    } else if (velocity <= vBucket * 6){
+      bucket[i] = 5;
+    } else if (velocity <= vBucket * 7){
+      bucket[i] = 6;
+    } else if (velocity <= vBucket * 8){
+      bucket[i] = 7;
+    } else if (velocity <= vBucket * 9){
+      bucket[i] = 8;
+    } else {
+      bucket[i] = 9;
+    }
+  }
+
+  const double threshold = 0.01*0.01;
+  maxV   = 0.0;
+  minDx  = std::numeric_limits<double>::max();
+
+  for (int j=0; j<NumberOfBodies; j++){ //loop through all particles
+    int timesteps = pow(2,bucket[j]);
     double alteredTime = timeStepSize/timesteps;
-    for(int step=0; step<timesteps; step++){
-      for (int jj=0; jj<bucketPointers[k]; jj++){ //loop through all particles in bucket
-        int j = buckets[k][jj];
-        if(j != -1){
-          force0[j] = 0.0;
-          force1[j] = 0.0;
-          force2[j] = 0.0;
-          for (int i=0; i<NumberOfBodies; i++) { //for each other particle do
-            if (i != j){
-              //storing calculations that are use multiple times to avoid redundancy
-              const double dist0 = x[i][0]-x[j][0], dist1 = x[i][1]-x[j][1], dist2 = x[i][2]-x[j][2];
-              const double distance = sqrt(dist0*dist0 + dist1*dist1 + dist2*dist2);
-
-              // x,y,z forces acting on particles
-              
-              const double a = mass[i]*mass[j] /(distance * distance * distance) ; 
-              //effect of other paritcle on current particle
-              const double f0 = dist0 * a, f1 = dist1 * a, f2 = dist2 * a;
-              force0[j] += f0 ;
-              force1[j] += f1 ;
-              force2[j] += f2 ;
-              minDx = std::min( minDx,distance );
-            }
-          }
-          x[j][0] = x[j][0] + alteredTime * v[j][0];
-          x[j][1] = x[j][1] + alteredTime * v[j][1];
-          x[j][2] = x[j][2] + alteredTime * v[j][2];
-          //Using Adams-Bashforth for velocity to increase accuracy
-          //not used if the first iteration or if a collision happened in the previous iteration (as the prevForce value will be wrong)
-          if(t<0 && !prevCol){
-
-            v[j][0] = v[j][0] + alteredTime*(1.5 * force0[j]/mass[j] - 0.5 * prevf0[j]/mass[j]);
-            v[j][1] = v[j][1] + alteredTime*(1.5 * force1[j]/mass[j] - 0.5 * prevf1[j]/mass[j]);
-            v[j][2] = v[j][2] + alteredTime*(1.5 * force2[j]/mass[j] - 0.5 * prevf2[j]/mass[j]);
-
-          }else{
-
-            v[j][0] = v[j][0] + alteredTime * force0[j] / mass[j];
-            v[j][1] = v[j][1] + alteredTime * force1[j] / mass[j];
-            v[j][2] = v[j][2] + alteredTime * force2[j] / mass[j];
-            prevCol = false;
-          }
-          maxV = std::max(maxV,std::sqrt( v[j][0]*v[j][0] + v[j][1]*v[j][1] + v[j][2]*v[j][2] ));
-          prevf0[j] = force0[j];
-          prevf1[j] = force1[j];
-          prevf2[j] = force2[j];
-        }
-      }
-      //collision checker
-      for (int j=0; j<NumberOfBodies; j++){
-        for(int i=j+1; i<NumberOfBodies; i++){
+    for (int k=0; k<timesteps; k++){
+      force0[j] = 0.0;
+      force1[j] = 0.0;
+      force2[j] = 0.0;
+      for (int i=0; i<NumberOfBodies; i++) { //for each other particle do 
+        if(i!=j){
+          //storing calculations that are use multiple times to avoid redundancy
           const double dist0 = x[i][0]-x[j][0], dist1 = x[i][1]-x[j][1], dist2 = x[i][2]-x[j][2];
           const double squareDist = dist0*dist0 + dist1*dist1 + dist2*dist2;
-          if (squareDist <= threshold){ 
+          const double distance = sqrt(squareDist);
+          // x,y,z forces acting on particles
+          if(squareDist<=threshold){
+            
             const double newMass = mass[i]+mass[j];
             for(int n=0; n<3; n++){ 
               v[j][n] = (mass[i]*v[i][n] + mass[j]*v[j][n])*(1/newMass);
@@ -310,25 +248,52 @@ void updateBody() {
             }
             mass[j] = newMass;
 
-            if(i!=NumberOfBodies-1){//if the collided particle is not the end one, store the end particles data in place of it (as we are no longer using it)
-              mass[i] = mass[NumberOfBodies-1];
-              x[i] = x[NumberOfBodies-1];
-              v[i] = v[NumberOfBodies-1];
+            for(int ii=i; ii<NumberOfBodies; ii++){ //j is the new particle so delete i by moving everything down by one
+              mass[ii] = mass[ii+1];
+              x[ii] = x[ii+1];
+              v[ii] = v[ii+1];
             }
+            NumberOfBodies -= 1; //decrement the number of particles 
+            i-= 1; //everything has been moved down by 1 so need to account for it
             prevCol = true;
-            for(int bucket=0; bucket<bucketNumber; bucket++){
-              for(int particle=0; particle<bucketPointers[bucket]; particle++){
-                if(buckets[bucket][particle] = i){
-                  buckets[bucket][particle] = -1;
-                } else if (buckets[bucket][particle] = NumberOfBodies -1){
-                  buckets[bucket][particle] = i;
-                }
-              }
-            }
-            NumberOfBodies -= 1;
-
+          }else{
+            const double a = mass[i]*mass[j] /(distance * distance * distance) ; 
+            //effect of other paritcle on current particle
+            const double f0 = dist0 * a, f1 = dist1 * a, f2 = dist2 * a;
+            force0[j] += f0 ;
+            force1[j] += f1 ;
+            force2[j] += f2 ;
+            //effect of current particle on other particle
+            force0[i] -= f0 ;
+            force1[i] -= f1 ;
+            force2[i] -= f2 ;
+            minDx = std::min( minDx,distance );
           }
         }
+        
+        x[j][0] = x[j][0] + alteredTime * v[j][0];
+        x[j][1] = x[j][1] + alteredTime * v[j][1];
+        x[j][2] = x[j][2] + alteredTime * v[j][2];
+        
+        //Using Adams-Bashforth for velocity to increase accuracy
+        //not used if the first iteration or if a collision happened in the previous iteration (as the prevForce value will be wrong)
+        if(t<0 && !prevCol){
+
+          v[j][0] = v[j][0] + alteredTime*(1.5 * force0[j]/mass[j] - 0.5 * prevf0[j]/mass[j]);
+          v[j][1] = v[j][1] + alteredTime*(1.5 * force1[j]/mass[j] - 0.5 * prevf1[j]/mass[j]);
+          v[j][2] = v[j][2] + alteredTime*(1.5 * force2[j]/mass[j] - 0.5 * prevf2[j]/mass[j]);
+
+        }else{
+
+          v[j][0] = v[j][0] + alteredTime * force0[j] / mass[j];
+          v[j][1] = v[j][1] + alteredTime * force1[j] / mass[j];
+          v[j][2] = v[j][2] + alteredTime * force2[j] / mass[j];
+          prevCol = false;
+        }
+        maxV = std::max(maxV,std::sqrt( v[j][0]*v[j][0] + v[j][1]*v[j][1] + v[j][2]*v[j][2] ));
+        prevf0[j] = force0[j];
+        prevf1[j] = force1[j];
+        prevf2[j] = force2[j];
       }
     }
   }
@@ -336,19 +301,42 @@ void updateBody() {
   
   //collision checker, using the square of the distance as a threshold (less error is involved when not square rooting)
   
-  
+  // for (int j=NumberOfBodies-1; j>0; j--){ //loop through from end of list to make sure no particles are overlooked even after a collision occurs (incase of a 3 way collision)
+  //   for(int i=0; i<j; i++){
+  //     const double dist0 = x[i][0]-x[j][0], dist1 = x[i][1]-x[j][1], dist2 = x[i][2]-x[j][2];
+  //     const double squareDist = dist0*dist0 + dist1*dist1 + dist2*dist2;
+  //     if (squareDist <= threshold){ 
+  //       const double newMass = mass[i]+mass[j];
+  //       for(int n=0; n<3; n++){ 
+  //         v[i][n] = (mass[i]*v[i][n] + mass[j]*v[j][n])*(1/newMass);
+  //         x[i][n] = (x[i][n] + x[j][n]) * 0.5;
+  //       }
+  //       mass[i] = newMass;
+
+  //       if(j!=NumberOfBodies-1){//if the collided particle is not the end one, store the end particles data in place of it (as we are no longer using it)
+  //         mass[j] = mass[NumberOfBodies-1];
+  //         x[j] = x[NumberOfBodies-1];
+  //         v[j] = v[NumberOfBodies-1];
+  //       }
+  //       NumberOfBodies -= 1; //decrement the number of particles 
+  //       prevCol = true;
+  //     }
+  //   }
+  // }
   if (NumberOfBodies==1){
     std::cout <<  x[0][0] << ", " << x[0][1] << ", " << x[0][2] << std::endl;
     t=tFinal+1;   
   }
+  
+
+
 
   t += timeStepSize;
 
   delete[] force0;
   delete[] force1;
   delete[] force2;
-  delete[] buckets;
-  delete[] bucketPointers;
+  delete[] bucket;
 }
 
 
